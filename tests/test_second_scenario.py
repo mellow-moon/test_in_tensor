1333cb9377664436990f3398b3580b1adbf8a927
Mikhail <106676401+mellow-moon@users.noreply.github.com>
2023-09-30 12:05:00 +0500

    update test

--- tests/test_second_scenario.py
+++ tests/test_second_scenario.py
@@ -17,11 +17,15 @@ def test_second_scenario(driver):
     sbis_contacts_page = sbis_home_page.go_to_contacts_page()
 
     sbis_contacts_page.region_defined_correct()
+
     sbis_contacts_page.partners_list_presented_and_visible()
 
     sbis_contacts_page.change_region()
 
     sbis_contacts_page.region_changed_successfully()
+
     sbis_contacts_page.partners_list_changed_successfully()
+
     sbis_contacts_page.url_is_correct()
+    
     sbis_contacts_page.title_is_correct()
